a186a2865298f31dd62d9dd65553e775f71d6d59
DylanCheetah <dylan_the_cheetah@outlook.com>
2023-12-14 17:35:08 -0500

    Implemented customer login.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,13 +10,39 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  // Check if there's a user with the given name and password
+  const filteredUsers = users.filter((user) => (user.username === username && user.password === password));
+  return filteredUsers.length == 1;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Get username and password
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check that a username was given
+  if(!username) {
+    // Send 400 Bad Request
+    return res.status(400).json({message: "No username given."});
+  }
+
+  // Check that a password was given
+  if(!password) {
+    // Send 400 Bad Request
+    return res.status(400).json({message: "No password given."});
+  }
+  
+  // Authenticate the user
+  if(authenticatedUser(req.body.username, req.body.password)) {
+    // Generate token and store it in the session
+    const token = jwt.sign(req.body, "user_fingerprint");
+    req.session.user = token;
+    return res.json({message: "Successfully logged in."});
+  } else {
+    // Send 400 Bad Request
+    return res.status(400).json({message: "Invalid credentials."});
+  }
 });
 
 // Add a book review
