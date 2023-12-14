cdfcb070569ed9e5eceff0c018edb8950261f696
DylanCheetah <dylan_the_cheetah@outlook.com>
2023-12-14 16:30:32 -0500

    Implemented get reviews.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,8 +42,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Send the reviews for the book identified by the given ISBN
+  return res.json(books[req.params.isbn].reviews);
 });
 
 module.exports.general = public_users;
